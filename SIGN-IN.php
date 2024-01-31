eb77d02a3f24e0a0e147de1bb02d1894eef2a726
odongojamachi1 <odongojamachi1@gmail.com>
2024-01-31 20:06:05 +0300

    why are you there

---
+++ SIGN-IN.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<head>
+    <title>
+        hello
+    </title>
+</head>
+<body>
+<nav class="navbar navbar-expand-sm bg-dark">
+            <div class="container-fluid">
+                  <ul class="navbar-nav">
+                        <ul class="nav">
+
+
+                         <li class="nave-item">
+                              <a class="nav-link" href="HOME.html">HOME</a>
+                                  </li>
+
+                                      <li class="nave-item">
+                                   <a class="nav-link" href="forms.html">FORMS</FORM></a>
+                                 </li>
+
+                               <li class="nave-item">
+                           <a class="nav-link" href="SERVICE.html">SERVICE</a>
+                         </li>
+
+                    <li class="nave-item">
+                <a class="nav-link" href="ABOUT.html">ABOUT</a>
+
+                <li class="nave-item">
+                <a class="nav-link" href="SIGN-IN.html">SIGN-IN</a>
+            </li></ul></nav>
+    <div class="container-fluid"
+    <center>
+        <H1>JAMACHI INNOVATVE TECHNOLOGIES</H1>
+    <P>SIGN IN TO OUR SERVICES</P>
+<form action="proccess.php" method="GET">
+    <input name="" placeholder="first name">
+    <input name="number" placeholder="password">
+    <button type="submit">submit</button>
+</form>
+</center>
+</div>
+</body>
